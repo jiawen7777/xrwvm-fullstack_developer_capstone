5849512223c85459fe72a1b1f3a8acb35b222ae3
jiawen <e-wjg@outlook.com>
2024-02-21 12:06:11 -0500

    Add dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -24,7 +24,8 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
+        print(e)
         # If any error occurs
         print("Network exception occurred")
 
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     # get dealer detail
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # get dealer reviews
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details')
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # add review
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
